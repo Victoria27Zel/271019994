e928a879632df9c29805daf1b0fd8d30db40099b
Victoria27Zel <tereshchenkoviktoria27@gmail.com>
2022-12-02 13:51:00 +0300

    add changes

---
+++ src/js/main.js
@@ -0,0 +1,17 @@
+//here will by JavaScript code ...
+function add(num1, num2){
+    return num1, num2;
+}
+function addNumber(num1){
+    const param = num1;
+    tpm = num1;
+    const smth = 3;
+
+     return function(num2){
+        return tmp + num2;
+     }
+
+}
+ let addtwo = addNumber(2);
+ let addfive = addNumber(25);
+ 
